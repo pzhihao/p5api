2e3c804d4c810c538c5cb8313b39997eac9328b8
pzhihao <864300572@qq.com>
2016-06-05 00:32:35 +0800

    添加Hibernate包--http://hibernate.org/orm/downloads/ 记录上回添加--Spring包  https://repo.spring.io spring-framework-4.0.0.RELEASE c3p0-0.9.1.2/com.springsource.net.sf.cglib-2.2.0/com.springsource.org.aopalliance-1.0.0/com.springsource.org.aspectj.weaver-1.6.8.RELEASE/commons-logging-1.1.3
    安装JBoss Tools/Spring IDE插件

--- src/com/pzhihao/servlet/MyServlet.java
+++ src/com/pzhihao/servlet/MyServlet.java
@@ -36,6 +36,7 @@ public class MyServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		
 	}
 
 }
